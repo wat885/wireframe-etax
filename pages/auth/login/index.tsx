56d96587a7e229f0d7e0b08bcedbb88c157989f3
watcharinta <watcharin.ta@os.ntplc.co.th>
2023-06-30 11:49:27 +0700

    edit image

--- pages/auth/login/index.tsx
+++ pages/auth/login/index.tsx
@@ -1,72 +1,123 @@
 /* eslint-disable @next/next/no-img-element */
 
-import { useRouter } from 'next/router';
-import React, { useContext, useState } from 'react';
-import AppConfig from '../../../layout/AppConfig';
-import { Checkbox } from 'primereact/checkbox';
-import { Button } from 'primereact/button';
-import { Password } from 'primereact/password';
-import { LayoutContext } from '../../../layout/context/layoutcontext';
-import { InputText } from 'primereact/inputtext';
-import { classNames } from 'primereact/utils';
-import { Page } from '../../../types/types';
+import { useRouter } from "next/router";
+import React, { useContext, useState } from "react";
+import AppConfig from "../../../layout/AppConfig";
+import { Checkbox } from "primereact/checkbox";
+import { Button } from "primereact/button";
+import { Password } from "primereact/password";
+import { LayoutContext } from "../../../layout/context/layoutcontext";
+import { InputText } from "primereact/inputtext";
+import { classNames } from "primereact/utils";
+import { Page } from "../../../types/types";
 
 const LoginPage: Page = () => {
-    const [password, setPassword] = useState('');
-    const [checked, setChecked] = useState(false);
-    const { layoutConfig } = useContext(LayoutContext);
+  const [password, setPassword] = useState("");
+  const [checked, setChecked] = useState(false);
+  const { layoutConfig } = useContext(LayoutContext);
 
-    const router = useRouter();
-    const containerClassName = classNames('surface-ground flex align-items-center justify-content-center min-h-screen min-w-screen overflow-hidden', { 'p-input-filled': layoutConfig.inputStyle === 'filled' });
+  const router = useRouter();
+  const containerClassName = classNames(
+    "surface-ground flex align-items-center justify-content-center min-h-screen min-w-screen overflow-hidden",
+    { "p-input-filled": layoutConfig.inputStyle === "filled" }
+  );
 
-    return (
-        <div className={containerClassName}>
-            <div className="flex flex-column align-items-center justify-content-center">
-                {/* <img src={`/layout/images/logo-${layoutConfig.colorScheme === 'light' ? 'dark' : 'white'}.svg`} alt="Sakai logo" className="mb-5 w-6rem flex-shrink-0" /> */}
-                <img src="http://101.109.250.52/NT-Logo/04_NT-Logo-with-Full-Name.png" alt="Sakai logo"  height="100" />
-                <div style={{ borderRadius: '56px', padding: '0.3rem', background: 'linear-gradient(180deg, var(--primary-color) 10%, rgba(33, 150, 243, 0) 30%)' }}>
-                    <div className="w-full surface-card py-8 px-5 sm:px-8" style={{ borderRadius: '53px' }}>
-                        <div className="text-center mb-5">
-                            {/* <img src="/demo/images/login/avatar.png" alt="Image" height="50" className="mb-3" /> */}
-                            <div className="text-900 text-3xl font-medium mb-3">E-Tax Invoice & Receipt</div>
-                            <span className="text-600 font-medium">Signin to E-Tax</span>
-                        </div>
+  return (
+    <div className={containerClassName}>
+      <div className="flex flex-column align-items-center justify-content-center">
+        {/* <img src={`/layout/images/logo-${layoutConfig.colorScheme === 'light' ? 'dark' : 'white'}.svg`} alt="Sakai logo" className="mb-5 w-6rem flex-shrink-0" /> */}
+        <img
+          src={`/layout/images/04_NT-Logo-with-Full-Name (1).png`}
+          alt="NT logo"
+          height="100"
+        />
+        <br/>
 
-                        <div>
-                            <label htmlFor="email1" className="block text-900 text-xl font-medium mb-2">
-                                Email
-                            </label>
-                            <InputText id="email1" type="text" placeholder="Email address" className="w-full md:w-30rem mb-5" style={{ padding: '1rem' }} />
+        {/* <img
+          alt="Prime Blocks"
+          className="w-full mt-3"
+          src={`/layout/images/banner-primeblocks.png`}
+        /> */}
+        {/* <img src="http://101.109.250.52/NT-Logo/04_NT-Logo-with-Full-Name.png" alt="NT logo"  height="100" /> */}
+        <div
+          style={{
+            borderRadius: "56px",
+            padding: "0.3rem",
+            background:
+              "linear-gradient(180deg, var(--primary-color) 10%, rgba(33, 150, 243, 0) 30%)",
+          }}
+        >
+          <div
+            className="w-full surface-card py-8 px-5 sm:px-8"
+            style={{ borderRadius: "53px" }}
+          >
+            <div className="text-center mb-5">
+              {/* <img src="/demo/images/login/avatar.png" alt="Image" height="50" className="mb-3" /> */}
+              <div className="text-900 text-3xl font-medium mb-3">
+                E-Tax Invoice & Receipt
+              </div>
+              <span className="text-600 font-medium">Signin to E-Tax</span>
+            </div>
 
-                            <label htmlFor="password1" className="block text-900 font-medium text-xl mb-2">
-                                Password
-                            </label>
-                            <Password inputId="password1" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Password" toggleMask className="w-full mb-5" inputClassName="w-full p-3 md:w-30rem"></Password>
+            <div>
+              <label
+                htmlFor="email1"
+                className="block text-900 text-xl font-medium mb-2"
+              >
+                Email
+              </label>
+              <InputText
+                id="email1"
+                type="text"
+                placeholder="Email address"
+                className="w-full md:w-30rem mb-5"
+                style={{ padding: "1rem" }}
+              />
 
-                            <div className="flex align-items-center justify-content-between mb-5 gap-5">
-                                {/* <div className="flex align-items-center">
+              <label
+                htmlFor="password1"
+                className="block text-900 font-medium text-xl mb-2"
+              >
+                Password
+              </label>
+              <Password
+                inputId="password1"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                placeholder="Password"
+                toggleMask
+                className="w-full mb-5"
+                inputClassName="w-full p-3 md:w-30rem"
+              ></Password>
+
+              <div className="flex align-items-center justify-content-between mb-5 gap-5">
+                {/* <div className="flex align-items-center">
                                     <Checkbox inputId="rememberme1" checked={checked} onChange={(e) => setChecked(e.checked ?? false)} className="mr-2"></Checkbox>
                                     <label htmlFor="rememberme1">Remember me</label>
                                 </div>
                                 <a className="font-medium no-underline ml-2 text-right cursor-pointer" style={{ color: 'var(--primary-color)' }}>
                                     Forgot password?
                                 </a> */}
-                            </div>
-                            <Button label="Sign In" className="w-full p-3 text-xl" onClick={() => router.push('/')}></Button>
-                        </div>
-                    </div>
-                </div>
+              </div>
+              <Button
+                label="Sign In"
+                className="w-full p-3 text-xl"
+                onClick={() => router.push("/")}
+              ></Button>
             </div>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 LoginPage.getLayout = function getLayout(page) {
-    return (
-        <React.Fragment>
-            {page}
-            <AppConfig simple />
-        </React.Fragment>
-    );
+  return (
+    <React.Fragment>
+      {page}
+      <AppConfig simple />
+    </React.Fragment>
+  );
 };
 export default LoginPage;
