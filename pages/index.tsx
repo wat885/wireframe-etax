c8e2e3717345022d9762374b10efe755353d80f4
watcharinta <watcharin.ta@os.ntplc.co.th>
2023-06-30 11:40:01 +0700

    fix: userdashboard

--- pages/index.tsx
+++ pages/index.tsx
@@ -123,7 +123,8 @@ const Dashboard = () => {
 
     return (
         <div className="grid">
-            <div className="col-12 lg:col-6 xl:col-3">
+            userdashboard
+            {/* <div className="col-12 lg:col-6 xl:col-3">
                 <div className="card mb-0">
                     <div className="flex justify-content-between mb-3">
                         <div>
@@ -379,7 +380,9 @@ const Dashboard = () => {
                         </Link>
                     </div>
                 </div>
-            </div>
+            </div> */}
+
+
         </div>
     );
 };
